6b99262797b3c112f31d8f5631884a06daeac7bc
kevin0309 <32089450+kevin0309@users.noreply.github.com>
2019-10-29 09:17:57 +0900

    delete scv proj

--- eclipse_workspace/SCV/src/framework/util/windowsAppProcessing/WindowsAppProcessOptions.java
+++
@@ -1,9 +0,0 @@
-package framework.util.windowsAppProcessing;
-
-import org.json.simple.JSONArray;
-
-public interface WindowsAppProcessOptions {
-
-	public String[] generateCmdLine();
-	public JSONArray generateJSONCmdLine();
-}
